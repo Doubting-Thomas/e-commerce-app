7aa36b12ca2f2ee961b185999183c00a2e95bd80
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-01-24 03:51:30 -0800

    chore: remove test/unused code

--- src/js/config.js
+++ src/js/config.js
@@ -9,15 +9,3 @@ export const cartDisplay = document.querySelector(".cart");
 export const cartContainer = document.querySelector(".cart__container");
 export const closeCart = document.querySelector(".btn-cart-close");
 console.log(navCartIcon);
-
-// navBookmarksIcon.addEventListener("click", function () {
-//   console.log("clicked");
-//   // cartDisplay.classList.toggle("transparent");
-//   // cartContainer.classList.toggle("reveal");
-// });
-
-// navCartIcon.addEventListener("click", function () {
-//   cartDisplay.classList.toggle("transparent");
-//   cartContainer.classList.toggle("reveal");
-//   console.log("togglers");
-// });
