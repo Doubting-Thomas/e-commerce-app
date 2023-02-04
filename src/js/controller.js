e563eff939bde952486ec5ca904ef68c544a9793
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-02-04 13:48:40 -0800

    chore: modify/clean up file imports

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,10 +3,8 @@ import { scrollAnimation } from "./smoothScroll";
 import { renderStickyNavbar } from "./stickyNav";
 import { renderAllSections } from "./scrollAnimation";
 import { renderCartDisplay, closeCartDisplay } from "./model.js";
-import { displayCart } from "./cart/renderCart.js";
-import addProducts, { addCart, addCartItem } from "./cart/addCart.js";
-import RenderCart from "./cart/renderCart.js";
-import { cartNames, cartTotal } from "./cart/totalCart.js";
+=
+import { addCart } from "./cart/addCart.js";
 import { btnClearCart } from "./config.js";
 import { clearCartItems } from "./cart/clearCart.js";
 
@@ -17,19 +15,7 @@ function init() {
   //renderAllSections();
   renderCartDisplay();
   closeCartDisplay();
-  //displayCart.renderProducts();
-  //addCart.addProduct();
-  // addCartItem.addHandlerRender();
   addCart();
-  //cartTotal();
-  //cartNames();
-  //addBag.renderProducts()
-  //displayCart.addCarts();
-  //const displayCart = new RenderCart();
-  //displayCart.renderProducts();
-
-  //renderCart.renderProducts();
-  //displayCart.renderProducts();
 }
 init();
 
