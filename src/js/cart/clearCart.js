7c56ab2b2ac3dff36c3b1a514f1e5c9bc913df4e
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-02-09 14:14:03 -0800

    feat: functionality to clear all cart items

--- src/js/cart/clearCart.js
+++ src/js/cart/clearCart.js
@@ -1,7 +1,8 @@
 import { cart } from "../model";
 import { cartCount, cartInfo, cartTotalPrice } from "../config";
-import { removeCartItems } from "./removeCart";
 import { cartTotal } from "./totalCart";
+import { removeCartProducts } from "../views/productsView";
+import { updateCartView } from "../views/productsView";
 
 export function clearCartItems() {
   let items = cart.map(function (item) {
@@ -9,13 +10,14 @@ export function clearCartItems() {
   });
 
   items.forEach(function (id) {
-    removeCartItems(id);
+    removeCartProducts(id);
   });
 
   while (cartInfo.children.length > 0) {
     cartInfo.removeChild(cartInfo.children[0]);
   }
 
-  cartCount.textContent = "";
-  cartTotalPrice.textContent = "";
+  // cartCount.textContent = "";
+  // cartTotalPrice.textContent = "";
+  updateCartView();
 }
