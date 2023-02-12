3190e6d3aa8c636786af702c31ae0e7fbe74d45f
Doubting-Thomas <thomasmichaelayo@gmail.com>
2023-02-12 14:08:59 -0800

    chore: modify file imports

--- src/js/model/cart/addCart.js
+++ src/js/model/cart/addCart.js
@@ -1,7 +1,6 @@
 import { updateCartView } from "./updateCart";
 import { cart } from "../model";
 import { items } from "../../products.js";
-//import { updateCartView } from "./updateCart";
 
 export function addCartProducts(id) {
   if (cart.some((product) => product.id === id)) {
